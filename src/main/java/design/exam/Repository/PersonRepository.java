bd498574dadc12e913bcb088aa9afdd6ad7cc498
“YOUR <YOUR>
2018-12-14 14:36:05 +0100

    T1.5 og T1.4

--- src/main/java/design/exam/Repository/PersonRepository.java
+++ src/main/java/design/exam/Repository/PersonRepository.java
@@ -1,10 +1,15 @@
 package design.exam.Repository;
 
+import design.exam.Model.Equipment;
 import design.exam.Model.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
     Person findByEmail(String email);
+    List<Equipment> findAllByFavoriteEquipment(Person person);
+
 }
